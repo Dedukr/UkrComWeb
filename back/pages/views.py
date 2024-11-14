a124e28cb731e2288aa45c03263b0d2bb7560054
Ruslan Ded <konozrusik@gmail.com>
2024-11-14 15:08:27 +0000

    Added react

---
+++ back/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+)
+
+# Create your views here.
+
